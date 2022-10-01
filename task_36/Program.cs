c63d5ca00c592d444f9221f24fce13d3267fd651
anwind-git <work-git@yandex.ru>
2022-10-01 23:36:46 +0300

    одномерный массив, заполненный случайными числами. Показывает сумму элементов с нечётными индексами.

---
+++ task_36/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+
+void Task36()
+{
+    int[] array = new int[5];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(-100, 100);
+    string arr = string.Join(", ", array);
+    PrintAmount(array, arr);
+}
+
+void PrintAmount(int[] array, string arr)
+{
+    int amount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 > 0)
+        {
+            amount += array[i];
+        }
+    }
+    Console.WriteLine($"Массив чисел: [" + arr + "]");
+    Console.WriteLine($"Сумма элементов с нечётными индексами: " + amount);
+}
+Task36();
